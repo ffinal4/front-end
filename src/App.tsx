ee04b29c74d0cfcb373a4026d6e32ddd4c4db78a
박준영 <pjy93525@gmail.com>
2023-07-31 21:31:13 +0900

    PEEPPO <package.json setting>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/common/Header";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<div>로그인페이지임</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>헤덩 ㅋ</div>;
+};
+
+export default Header;
