967a5ef432f89dbc5274a78f81360c5ac68dfb4f
seo myeong jin <seomyeongjin99@gmail.com>
2023-08-01 16:54:46 +0900

    "PEEP-6 <LogIn 구현중>"

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
 import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import SearchInput from "./components/common/SearchInput";
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <BrowserRouter>
+      <Header />
+      <SearchInput />
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<div>로그인페이지임!</div>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,45 +1,72 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <HeadContainer>
       <FirstContainer>
-        <Logo>로고</Logo>
-        <div>주머니보기</div>
-        <div>주변둘러보기</div>
+        <Logo
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          로고
+        </Logo>
+        <SecondMenu>주머니핍핑</SecondMenu>
+        <ThirdMenu>주변둘러보기</ThirdMenu>
       </FirstContainer>
       <SecondContainer>
-        <div>로그인</div>
-        <div>화가</div>
-        <div>내주머니ddddd</div>
+        <div
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          로그인
+        </div>
+        <div
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          회원가입
+        </div>
+        <div>내주머니</div>
       </SecondContainer>
     </HeadContainer>
   );
 };
 
 const HeadContainer = styled.div`
-  border: 3px solid #cadeed;
   width: 100%;
-  height: 70px;
+  height: 76px;
+  margin: auto;
   display: flex;
   align-items: center;
   justify-content: space-evenly;
+  font-size: 18px;
+  border-bottom: 1px solid gray;
 `;
 
 const FirstContainer = styled.div`
-  border: 2px solid red;
+  /* border: 2px solid red; */
   width: 540px;
   display: flex;
   justify-content: space-evenly;
 `;
 
 const SecondContainer = styled.div`
-  border: 2px solid blue;
-  width: 222px;
+  /* border: 2px solid blue; */
+  width: 300px;
   display: flex;
   justify-content: space-evenly;
 `;
 
-const Logo = styled.img``;
+const Logo = styled.div``;
+
+const SecondMenu = styled.div`
+  cursor: pointer;
+`;
+const ThirdMenu = styled.div``;
 export default Header;
