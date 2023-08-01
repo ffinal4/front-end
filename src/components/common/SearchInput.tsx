eb68b424f92d9ede7a7461f54ca8159013f0332a
seo myeong jin <seomyeongjin99@gmail.com>
2023-08-01 16:55:10 +0900

    "PEEP-6 <로그인페이지 구현중>"

---
+++ src/components/common/SearchInput.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const SearchInput = () => {
+  return (
+    <SearchContainer>
+      <InputContainer>
+        <Input type="search" placeholder="search" />
+      </InputContainer>
+    </SearchContainer>
+  );
+};
+const SearchContainer = styled.div`
+  border-bottom: 1px solid gray;
+  width: 100%;
+  height: 76px;
+  display: flex;
+`;
+const InputContainer = styled.div`
+  /* border: 1px solid red; */
+  width: 800px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+`;
+const Input = styled.input`
+  border: 1px solid gray;
+  width: 754px;
+  height: 44px;
+  font-size: 18px;
+  padding: 10px;
+`;
+export default SearchInput;
