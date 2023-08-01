c53683f12fdc63cd1a9da4f70fed3b5d3f59ca30
DESKTOP-VDHMJ06\seungjae <sean2684@naver.com>
2023-08-01 16:55:38 +0900

    PEEP-15 <상세페이지 작업 중>

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
 import MainPage from "./pages/MainPage";
 import UploadPage from "./pages/UploadPage";
+import DetailPage from "./pages/DetailPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<div>로그인페이지임!</div>} />
           <Route path="/upload" element={<UploadPage />} />
+          <Route path="/detail" element={<DetailPage />} />
         </Routes>
       </BrowserRouter>
     </div>
