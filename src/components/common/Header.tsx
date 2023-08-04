56b73488d565de5e961c462622945f1cc82a1365
seo myeong jin <seomyeongjin99@gmail.com>
2023-08-04 21:57:44 +0900

    PEEP-16 <Header 상단바 고정>

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -28,6 +28,11 @@ const Header = () => {
 export default Header;
 
 const HeadContainer = styled.div`
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+  right: 0;
   width: 100%;
   height: 70px;
   display: flex;
