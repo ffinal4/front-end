fa78639b5bee1f96a23c6bdb1cc1820ac96508ff
seo myeong jin <seomyeongjin99@gmail.com>
2023-08-11 03:49:06 +0900

    PEEP 33 <교환 요청 페이지 마크업 진행중>

--- src/pages/TradeRequestPage.tsx
+++ src/pages/TradeRequestPage.tsx
@@ -1,7 +1,182 @@
 import React from "react";
+import { styled } from "styled-components";
+import image from "../assets/images/ppapparo.jpg";
 
 const TradeRequestPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Title>TRADING REQUEST</Title>
+      <Container>
+        <SubTitle>교환요청리스트</SubTitle>
+        <Filter>Filter</Filter>
+      </Container>
+      <TradeRequestListContainer>
+        <MenuContainer>
+          <RequestDateMenu>요청일자</RequestDateMenu>
+          <TradeRequestItemMenu>교환 요청 물품</TradeRequestItemMenu>
+          <MyItemMenu>내 물건</MyItemMenu>
+          <TradeStateMenu>상태</TradeStateMenu>
+          <ChatState />
+        </MenuContainer>
+        <RequestContainer>
+          <RequestDate>2023.07.30</RequestDate>
+          <TradeRequestItem>
+            <RequestItemImgContainer>
+              <Image src={image} />
+              <ContentContainer>
+                <ItemTitle>
+                  스파이더맨 어크로스 더 유니버스 IMAX 포스터
+                </ItemTitle>
+                <User>바꾼사람이핍포</User>
+              </ContentContainer>
+            </RequestItemImgContainer>
+          </TradeRequestItem>
+        </RequestContainer>
+      </TradeRequestListContainer>
+    </div>
+  );
 };
 
+const Title = styled.div`
+  border: 1px solid red;
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 110%;
+`;
+
+const Container = styled.div`
+  border: 1px solid red;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 16px;
+`;
+const SubTitle = styled.div`
+  border: 1px solid red;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 150%;
+`;
+
+const Filter = styled.div`
+  border: 1px solid #222020;
+  width: 176px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  padding: 10px 10px 10px 20px;
+`;
+
+const TradeRequestListContainer = styled.div`
+  border: 3px solid black;
+  margin-top: 20px;
+  width: 1136px;
+  height: 1194px;
+`;
+
+const MenuContainer = styled.div`
+  /* border: 1px solid green; */
+  background-color: #efefef;
+  display: flex;
+  height: 56px;
+`;
+
+const RequestDateMenu = styled.div`
+  /* border: 1px solid blue; */
+  width: 174px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const TradeRequestItemMenu = styled.div`
+  border: 1px solid blue;
+  width: 478px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const MyItemMenu = styled.div`
+  border: 1px solid blue;
+  width: 112px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const TradeStateMenu = styled.div`
+  border: 1px solid blue;
+  width: 172px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+const ChatState = styled.div`
+  /* border: 1px solid blue; */
+  width: 192px;
+`;
+
+const RequestContainer = styled.div`
+  border: 1.5px solid green;
+  height: 112px;
+  display: flex;
+  align-items: center;
+`;
+
+const RequestItemImgContainer = styled.div`
+  border: 1px solid blue;
+  width: 80px;
+  height: 80px;
+  display: flex;
+`;
+
+const Image = styled.img`
+  border: 1px solid black;
+  width: 80px;
+  height: 80px;
+`;
+
+const ContentContainer = styled.div`
+  border: 3px solid green;
+`;
+const RequestDate = styled.div`
+  border: 1px solid red;
+  width: 174px;
+  height: 112px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TradeRequestItem = styled.div`
+  border: 1px solid red;
+  width: 478px;
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+`;
+
+const ItemTitle = styled.div`
+  border: 1px solid red;
+  width: 346px;
+  height: 24px;
+  display: flex;
+  border: 1px solid blue;
+`;
+
+const User = styled.div`
+  border: 1px solid blue;
+  width: 346px;
+`;
 export default TradeRequestPage;
