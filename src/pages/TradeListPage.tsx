5845aa12fae04fe7c9adaed1814f02529d05945e
박준영 <pjy93525@gmail.com>
2023-08-16 00:43:55 +0900

    PEEP-34 <Feat: react-query setting완료

--- src/pages/TradeListPage.tsx
+++ src/pages/TradeListPage.tsx
@@ -3,12 +3,20 @@ import { styled } from "styled-components";
 import eyeImage from "../assets/images/eye.svg";
 import HorizontalLine from "../components/common/HorizontalLine";
 import FilterButton from "../components/common/FilterButton";
-import ItemCard from "../components/common/ItemCard";
 import Paging from "../components/common/Paging/Paging";
 import { StTitle } from "../styles/TitleFont";
 import ItemCardList from "../components/common/ItemCardList";
+import { useQuery } from "react-query";
+import { getGoodsApi } from "../api/goods";
 
 const TradeListPage = () => {
+  const { isLoading, error, data } = useQuery("tradeListPageData", getGoodsApi);
+  if (isLoading) return <div>Loading...</div>;
+  console.log("물물교환페이지데이터", data);
+  if (error) {
+    console.log(error);
+  }
+
   return (
     <TradeListPageContainer>
       <TitleContainer>
