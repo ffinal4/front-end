f0296f72482d34c489fa72b45addb39028953bf1
seo myeong jin <seomyeongjin99@gmail.com>
2023-08-16 13:31:03 +0900

    PEEP-42 <Fixed : 회원가입 form 태그 수정완료>

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -72,139 +72,141 @@ const SignupPage = () => {
       <TitleContainer>
         <Title>READY TO PEEPO?</Title>
       </TitleContainer>
-      <EmailContainer>
-        <Label>이메일(아이디)</Label>
-        <EmailInputContainer>
-          <StBasicInput
-            borderColor="#ADADAD"
-            focusBorderColor="#EC0000"
-            type="email"
-            placeholder="이메일을 입력해주세요"
-            {...register("email", {
-              required: "필수입력 항목입니다.",
-              pattern: {
-                value: /^[a-zA-Z\d]{2,}$/,
-                message: "이미 사용중인 이메일입니다.",
-              },
-            })}
-          />
-        </EmailInputContainer>
-
-        <AtContainer>@</AtContainer>
-        <SelectContainer>
-          <EmailSelect
-            {...register("select", { required: "필수입력 항목입니다." })}
-          >
-            <option value="">선택해주세요</option>
-            <option value="@naver.com">naver.com</option>
-            <option value="@hanmail.net">hanmail.net</option>
-            <option value="@daum.net">daum.net</option>
-            <option value="@gmail.com">gmail.com</option>
-            <option value="@nate.com">nate.com</option>
-            <option value="@hotmail.com">hotmail.com</option>
-            <option value="@outlook.com">outlook.com</option>
-            <option value="@icloud.com">icloud.com</option>
-          </EmailSelect>
-        </SelectContainer>
-      </EmailContainer>
-      <ValidateMessage>{errors?.email?.message}</ValidateMessage>
-      <PwContainer>
-        <Label>비밀번호</Label>
-        <PwInputContainer>
-          <PwVisibleButton onClick={onClickPasswordType}>
-            {pwType.visible ? (
-              <PwImg src={closeeye} />
-            ) : (
-              <PwImg src={openeye} />
-            )}
-          </PwVisibleButton>
-          <StBasicInput
-            borderColor="#ADADAD"
-            focusBorderColor="#EC0000"
-            type={pwType.type}
-            placeholder="비밀번호를 입력해주세요."
-            {...register("password", {
-              required: "필수입력 항목입니다.",
-              minLength: {
-                value: 8,
-                message: "비밀번호는 8자 이상이어야 합니다.",
-              },
-              pattern: {
-                value: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/,
-                message:
-                  "영문, 숫자, 특수문자 각 1개 이상을 포함한 8자리 이상의 비밀번호를 작성해주세요.",
-              },
-            })}
-          />
-        </PwInputContainer>
-      </PwContainer>
-      <PwValidateMessage>{errors?.password?.message}</PwValidateMessage>
-      <CheckPwContainer>
-        <Label>비밀번호 확인</Label>
-        <CheckPwInputContainer>
-          <StBasicInput
-            borderColor="#ADADAD"
-            focusBorderColor="#EC0000"
-            type={pwType.type}
-            placeholder="비밀번호를 입력해주세요."
-            {...register("confirmPassword", {
-              required: "필수입력 항목입니다.",
-              validate: {
-                check: (value) => {
-                  if (getValues("password") !== value) {
-                    return "비밀번호가 일치하지 않습니다.";
-                  }
+      <SignUpContainer>
+        <EmailContainer>
+          <Label>이메일(아이디)</Label>
+          <EmailInputContainer>
+            <StBasicInput
+              borderColor="#ADADAD"
+              focusBorderColor="#EC0000"
+              type="email"
+              placeholder="이메일을 입력해주세요"
+              {...register("email", {
+                required: "필수입력 항목입니다.",
+                pattern: {
+                  value: /^[a-zA-Z\d]{2,}$/,
+                  message: "이미 사용중인 이메일입니다.",
                 },
-              },
-            })}
-          />
-        </CheckPwInputContainer>
-      </CheckPwContainer>
-      <CheckPwValidateMessage>
-        {errors?.confirmPassword?.message}
-      </CheckPwValidateMessage>
-      <AddressContainer>
-        <Label>주소</Label>
-        <AddressInputContainer>
-          <KakaoApi
-            address={address}
-            setAddress={setAddress}
-            openPostcode={openPostcode}
-            setOpenPostcode={setOpenPostcode}
-          />
-        </AddressInputContainer>
-      </AddressContainer>
-      <ContentContainer>
-        <AddressContent>
-          입력한 주소는 나의 주거래 지역으로 표시됩니다.
-        </AddressContent>
-      </ContentContainer>
+              })}
+            />
+          </EmailInputContainer>
 
-      <NickNameContainer>
-        <SecondLabel>닉네임</SecondLabel>
-        <NickNameInputContainer>
-          <StBasicInput
-            borderColor="#ADADAD"
-            focusBorderColor="#EC0000"
-            type="text"
-            placeholder="한글, 영문, 숫자를 이용한 2~15자"
-            {...register("nickname", {
-              required: "필수입력 항목입니다.",
-              minLength: { value: 2, message: "2자 이상 입력해주세요." },
-              maxLength: { value: 15, message: "15자 이하로 입력해주세요." },
-              pattern: {
-                value: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,15}$/,
-                message: "영문 대소문자, 글자 단위 한글, 숫자만 가능합니다.",
-              },
-            })}
-          />
-        </NickNameInputContainer>
+          <AtContainer>@</AtContainer>
+          <SelectContainer>
+            <EmailSelect
+              {...register("select", { required: "필수입력 항목입니다." })}
+            >
+              <option value="">선택해주세요</option>
+              <option value="@naver.com">naver.com</option>
+              <option value="@hanmail.net">hanmail.net</option>
+              <option value="@daum.net">daum.net</option>
+              <option value="@gmail.com">gmail.com</option>
+              <option value="@nate.com">nate.com</option>
+              <option value="@hotmail.com">hotmail.com</option>
+              <option value="@outlook.com">outlook.com</option>
+              <option value="@icloud.com">icloud.com</option>
+            </EmailSelect>
+          </SelectContainer>
+        </EmailContainer>
+        <ValidateMessage>{errors?.email?.message}</ValidateMessage>
+        <PwContainer>
+          <Label>비밀번호</Label>
+          <PwInputContainer>
+            <PwVisibleButton onClick={onClickPasswordType}>
+              {pwType.visible ? (
+                <PwImg src={closeeye} />
+              ) : (
+                <PwImg src={openeye} />
+              )}
+            </PwVisibleButton>
+            <StBasicInput
+              borderColor="#ADADAD"
+              focusBorderColor="#EC0000"
+              type={pwType.type}
+              placeholder="비밀번호를 입력해주세요."
+              {...register("password", {
+                required: "필수입력 항목입니다.",
+                minLength: {
+                  value: 8,
+                  message: "비밀번호는 8자 이상이어야 합니다.",
+                },
+                pattern: {
+                  value: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$/,
+                  message:
+                    "영문, 숫자, 특수문자 각 1개 이상을 포함한 8자리 이상의 비밀번호를 작성해주세요.",
+                },
+              })}
+            />
+          </PwInputContainer>
+        </PwContainer>
+        <PwValidateMessage>{errors?.password?.message}</PwValidateMessage>
+        <CheckPwContainer>
+          <Label>비밀번호 확인</Label>
+          <CheckPwInputContainer>
+            <StBasicInput
+              borderColor="#ADADAD"
+              focusBorderColor="#EC0000"
+              type={pwType.type}
+              placeholder="비밀번호를 입력해주세요."
+              {...register("confirmPassword", {
+                required: "필수입력 항목입니다.",
+                validate: {
+                  check: (value) => {
+                    if (getValues("password") !== value) {
+                      return "비밀번호가 일치하지 않습니다.";
+                    }
+                  },
+                },
+              })}
+            />
+          </CheckPwInputContainer>
+        </CheckPwContainer>
+        <CheckPwValidateMessage>
+          {errors?.confirmPassword?.message}
+        </CheckPwValidateMessage>
+        <AddressContainer>
+          <Label>주소</Label>
+          <AddressInputContainer>
+            <KakaoApi
+              address={address}
+              setAddress={setAddress}
+              openPostcode={openPostcode}
+              setOpenPostcode={setOpenPostcode}
+            />
+          </AddressInputContainer>
+        </AddressContainer>
+        <ContentContainer>
+          <AddressContent>
+            입력한 주소는 나의 주거래 지역으로 표시됩니다.
+          </AddressContent>
+        </ContentContainer>
 
-        <StBasicButton buttonColor="#D9D9D9;" style={{ marginLeft: "20px" }}>
-          중복 확인
-        </StBasicButton>
-      </NickNameContainer>
-      <ValidateMessage>{errors?.nickname?.message}</ValidateMessage>
+        <NickNameContainer>
+          <SecondLabel>닉네임</SecondLabel>
+          <NickNameInputContainer>
+            <StBasicInput
+              borderColor="#ADADAD"
+              focusBorderColor="#EC0000"
+              type="text"
+              placeholder="한글, 영문, 숫자를 이용한 2~15자"
+              {...register("nickname", {
+                required: "필수입력 항목입니다.",
+                minLength: { value: 2, message: "2자 이상 입력해주세요." },
+                maxLength: { value: 15, message: "15자 이하로 입력해주세요." },
+                pattern: {
+                  value: /^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,15}$/,
+                  message: "영문 대소문자, 글자 단위 한글, 숫자만 가능합니다.",
+                },
+              })}
+            />
+          </NickNameInputContainer>
+
+          <StBasicButton buttonColor="#D9D9D9;" style={{ marginLeft: "20px" }}>
+            중복 확인
+          </StBasicButton>
+        </NickNameContainer>
+        <ValidateMessage>{errors?.nickname?.message}</ValidateMessage>
+      </SignUpContainer>
       <AssignButtonContainer>
         <StBasicButton buttonColor="#D9D9D9" onClick={signupOnclick}>
           회원가입
@@ -228,8 +230,7 @@ const Title = styled.div`
   font-weight: 800;
   margin-bottom: 30px;
 `;
-const SignUpForm = styled.form`
-  /* border: 5px solid yellow; */
+const SignUpContainer = styled.div`
   border-top: 5px solid black;
   border-bottom: 5px solid black;
   max-width: 1136px;
@@ -401,7 +402,7 @@ const AssignButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   /* border: 1px solid red; */
-  padding: 20px 0px 20px 0px;
+  margin-top: 40px;
 `;
 
 export default SignupPage;
