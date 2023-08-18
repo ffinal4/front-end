5283b9168d4c9241bc25ebd79e4c54af417db0be
seo myeong jin <seomyeongjin99@gmail.com>
2023-08-18 21:24:42 +0900

    PEEP-58 <Feat : 로그아웃 버튼 눌렀을 때 헤더 변경>

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -15,7 +15,11 @@ import { postLogoutApi } from "../../api/users";
 const Header = () => {
   const navigate = useNavigate();
   const insertedToken: string | null = localStorage.getItem("accessToken");
-  useEffect(() => {}, [insertedToken]);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoggedIn(!!insertedToken);
+  }, [insertedToken]);
 
   const logoutOnclick = async () => {
     const accessToken = localStorage.getItem("accessToken");
@@ -29,6 +33,7 @@ const Header = () => {
     try {
       const res = await postLogoutApi(logoutData);
       console.log(res);
+      setIsLoggedIn(false);
     } catch (error) {
       console.log("로그아웃실패", error);
     }
@@ -43,7 +48,7 @@ const Header = () => {
               navigate("/");
             }}
           >
-            {insertedToken ? (
+            {isLoggedIn ? (
               <>
                 <Logo src={loginLogo} />
                 <LogoTitle src={loginLTitle} />
@@ -61,7 +66,7 @@ const Header = () => {
             </SearchButton>
             <SearchInput type="search" placeholder="Search" />
           </InputContainer>
-          {insertedToken ? (
+          {isLoggedIn ? (
             <AllIconContainer>
               <IconContainer>
                 <Alarm src={alarm} />
@@ -191,7 +196,6 @@ const LinkContainer = styled.div`
 
 const LoginLink = styled.div`
   cursor: pointer;
-  /* border: 1px solid blue; */
   margin-left: 20px;
   font-size: 16px;
   height: 24px;
@@ -215,13 +219,11 @@ const AllIconContainer = styled.div`
   font-family: Pretendard;
 `;
 const IconContainer = styled.div`
-  /* border: 1px solid red; */
   display: flex;
   align-items: center;
 `;
 
 const Alarm = styled.img`
-  /* border: 1px solid blue; */
   width: 24px;
   height: 24px;
   margin-right: 20px;
@@ -229,7 +231,6 @@ const Alarm = styled.img`
 `;
 
 const Mypage = styled.img`
-  /* border: 1px solid blue; */
   width: 24px;
   height: 24px;
   margin-right: 20px;
@@ -237,7 +238,6 @@ const Mypage = styled.img`
 `;
 
 const Peeppo = styled.img`
-  /* border: 1px solid blue; */
   width: 24px;
   height: 24px;
   cursor: pointer;
