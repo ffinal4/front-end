e9c16fd6a002dabb9673b484dc698f156b119d2d
seo myeong jin <seomyeongjin99@gmail.com>
2023-08-23 23:15:06 +0900

    PEEP-65 <Feat : 교환상태 공통부분 전역스타일로 설정

---
+++ src/styles/RequestStateBox.ts
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const StRequestState = styled.div<{ backgroundcolor: string }>`
+  width: 110px;
+  height: 44px;
+  border-radius: 0px 10px;
+  position: absolute;
+  right: 0;
+  font-family: "Pretendard";
+  font-size: 16px;
+  font-weight: 700;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${(props) => props.backgroundcolor};
+`;
