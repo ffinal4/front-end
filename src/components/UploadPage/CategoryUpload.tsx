8e5294dcba83ef1ddc9f28ebbc6ee6327fa433b8
박준영 <pjy93525@gmail.com>
2023-09-01 03:21:56 +0900

    PEEP-39 <Feat: FilterModatComponent 구현완료>

--- src/components/UploadPage/CategoryUpload.tsx
+++ src/components/UploadPage/CategoryUpload.tsx
@@ -1,108 +1,106 @@
-import React, { useState, useEffect } from 'react';
-import { styled } from 'styled-components'
-import CategorySelect from '../common/CategorySelect';
+import React, { useState, useEffect } from "react";
+import { styled } from "styled-components";
+import CategorySelect from "../common/CategorySelect";
 
-const CategoryUpload = ({ setUploadData, uploadData, failedUpload } : any) => {
+const CategoryUpload = ({ setUploadData, uploadData, failedUpload }: any) => {
+  const [categorySelect, setCategorySelect] = useState({
+    category: "",
+    name: "카테고리 선택",
+  });
+  const { category, name } = categorySelect;
+  const [selectBar, setSelectBar] = useState<boolean>(false);
 
-    const  [categorySelect, setCategorySelect] = useState({
-        category: "",
-        name: "카테고리 선택",
-    });
-    const { category, name } = categorySelect;
-    const [selectBar, setSelectBar] = useState<boolean>(false);
+  const onClickDropDownHandelr = () => {
+    setSelectBar(!selectBar);
+  };
 
-    const onClickDropDownHandelr = () => {
-        setSelectBar(!selectBar);
-    };
-
-    useEffect(() => {
-        if (category !== "") {
-            setUploadData({...uploadData, data: {...uploadData.data, category: category}});
-        };
-    }, [selectBar]);
+  useEffect(() => {
+    if (category !== "") {
+      setUploadData({ ...uploadData, data: { ...uploadData.data, category: category } });
+    }
+  }, [selectBar]);
 
   return (
     <LineContainer>
-        <RequiredText
-            style={{color: `${(failedUpload && uploadData.data.category === "") && "#DF3737"}`}}
-        >카테고리*</RequiredText>
-        <RightWrapper>
-            <SelectBar>
-                <Text>{name}</Text>
-                <ChoiceBox onClick={onClickDropDownHandelr}></ChoiceBox>
-            </SelectBar>
-            {(selectBar)
-            && <SelectContainer>
-                    <CategorySelect
-                        categorySelect={categorySelect}
-                        setCategorySelect={setCategorySelect}
-                        setSelectBar={setSelectBar}
-                    />
-                </SelectContainer>}
-            {(failedUpload && uploadData.data.category === "")
-                && <Text style={{color: "#DF3737"}}>
-                    * 카테고리를 선택해 주세요.
-                </Text>
-            }
-        </RightWrapper>
+      <RequiredText style={{ color: `${failedUpload && uploadData.data.category === "" && "#DF3737"}` }}>
+        카테고리*
+      </RequiredText>
+      <RightWrapper>
+        <SelectBar>
+          <Text>{name}</Text>
+          <ChoiceBox onClick={onClickDropDownHandelr}></ChoiceBox>
+        </SelectBar>
+        {selectBar && (
+          <SelectContainer>
+            <CategorySelect
+              categorySelect={categorySelect}
+              setCategorySelect={setCategorySelect}
+              setSelectBar={setSelectBar}
+            />
+          </SelectContainer>
+        )}
+        {failedUpload && uploadData.data.category === "" && (
+          <Text style={{ color: "#DF3737" }}>* 카테고리를 선택해 주세요.</Text>
+        )}
+      </RightWrapper>
     </LineContainer>
-  )
+  );
 };
 
-const LineContainer = styled.div`
-    width: 100%;
-    display: flex;
-    padding: 30px 0px 30px 0px;
-    border-bottom: 2px solid #EAEAEA;
+export const LineContainer = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 30px 0px 30px 0px;
+  border-bottom: 2px solid #eaeaea;
 `;
 
-const RequiredText = styled.div`
-    font-family: "Pretendard";
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 150%;
-    min-width: 191px;
+export const RequiredText = styled.div`
+  font-family: "Pretendard";
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 150%;
+  min-width: 191px;
 `;
 
-const SelectBar = styled.div`
-    display: inline-flex;
-    width: 176px;
-    height: 44px;
-    padding: 0px 10px;
-    border-bottom: 1px solid;
-    justify-content: space-between;
-    align-items: center;
+export const SelectBar = styled.div`
+  display: inline-flex;
+  width: 176px;
+  height: 44px;
+  padding: 0px 10px;
+  border-bottom: 1px solid;
+  justify-content: space-between;
+  align-items: center;
 
-    @media screen and (max-width: 843px) {
-        gap: 20px;
-    }
+  @media screen and (max-width: 843px) {
+    gap: 20px;
+  }
 `;
 
-const Text = styled.div`
-    font-family: "Pretendard";
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 150%;
+export const Text = styled.div`
+  font-family: "Pretendard";
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 150%;
 `;
 
-const ChoiceBox = styled.div`
-    width: 24px;
-    height: 24px;
-    background-color: #D9D9D9;
-    cursor: pointer;
+export const ChoiceBox = styled.div`
+  width: 24px;
+  height: 24px;
+  background-color: #d9d9d9;
+  cursor: pointer;
 `;
 
-const SelectContainer = styled.div`
-    padding: 40px 0px 0px 0px;
-    position: absolute;
-    z-index: 1000;
+export const SelectContainer = styled.div`
+  padding: 40px 0px 0px 0px;
+  position: absolute;
+  z-index: 1000;
 `;
 
-const RightWrapper = styled.div`
-    width: 100%;
-    display: grid;
-    gap: 10px;
-    position: relative;
+export const RightWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  gap: 10px;
+  position: relative;
 `;
 
 export default CategoryUpload;
---
+++ src/components/common/FilterModal.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const FilterModal = () => {
+  interface Kind {
+    category: string | null;
+    name: string;
+  }
+
+  const categoryArray: Kind[] = [
+    { category: null, name: "전체" },
+    { category: "WOMAN", name: "여성패션/잡화" },
+    { category: "MAN", name: "남성패션/잡화" },
+    { category: "FURNITURE", name: "가구/인테리어" },
+    { category: "HOBBY", name: "취미/게임/음반/굿즈" },
+    { category: "BOOK", name: "도서" },
+    { category: "BEAUTY", name: "뷰티/미용" },
+    { category: "BABY", name: "유아동/유아물품" },
+    { category: "KITCHEN", name: "생활주방용품" },
+    { category: "TICKET", name: "티켓/교환권" },
+    { category: "SPORTS", name: "스포츠/레저" },
+    { category: "PET", name: "반려동물용품" },
+    { category: "DIGITAL", name: "디지털기기" },
+    { category: "ELECTRONICS", name: "가전제품" },
+    { category: "ART", name: "예술/희귀/수집품" },
+    { category: "PLANT", name: "식물" },
+    { category: "FOOD", name: "가공식품" },
+    { category: "ETC", name: "기타물품" },
+  ];
+
+  return (
+    <CategoryContainer>
+      <CategoryHeader>전체카테고리</CategoryHeader>
+      <CategoryWrapper>
+        {categoryArray.map((item) => {
+          return (
+            <CategoryKindWrapper
+              key={item.category}
+              onClick={() => {
+                alert("hi");
+              }}
+            >
+              <Text>{item.name}</Text>
+            </CategoryKindWrapper>
+          );
+        })}
+      </CategoryWrapper>
+    </CategoryContainer>
+  );
+};
+
+const CategoryContainer = styled.div`
+  width: 1000px;
+  height: 310px;
+  position: absolute;
+  box-shadow: 1px 1px 8px 0px #c7c7c7;
+  background-color: white;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+`;
+
+const CategoryHeader = styled.div`
+  width: 100%;
+  height: 54px;
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  background-color: #39373a;
+  color: #fff;
+  font-family: "Pretendard";
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 150%;
+`;
+
+const CategoryWrapper = styled.div`
+  width: 100%;
+  height: 256px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const CategoryKindWrapper = styled.div`
+  display: flex;
+  width: 200px;
+  padding: 20px 0px 20px 20px;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+
+  &:hover {
+    font-weight: 700;
+    background-color: #efefef;
+  }
+`;
+
+const Text = styled.div`
+  display: flex;
+  font-family: "Pretendard";
+  font-size: 16px;
+  line-height: 150%;
+`;
+export default FilterModal;
